f3b303befc29631ecb3ed0a77f0a064477657e92
JamLee <1575103047@qq.com>
2023-03-12 16:43:59 +0800

    fix: backquota not work on mac

--- clean_build.sh
+++ clean_build.sh
@@ -8,13 +8,16 @@ if [ $? -eq 0 ]; then
 else
     B_CMAKE=cmake
 fi
+
+B_BUILD_TYPE=Release
+
 # default build configuration
 B_BUILD_TYPE=${B_BUILD_TYPE:-Debug}
 if [ "$(uname)" = "Darwin" ]; then
     # OSX needs a lot of extra help, poor thing
     # run the osx_environment.sh script to fix paths
     . ./osx_environment.sh
-    B_CMAKE_FLAGS="-DCMAKE_OSX_SYSROOT=$(xcode-select --print-path)/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 $B_CMAKE_FLAGS"
+    B_CMAKE_FLAGS="-DCMAKE_OSX_SYSROOT=$(xcode-select --print-path)/SDKs/MacOSX.sdk -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 $B_CMAKE_FLAGS"
 fi
 # allow local customizations to build environment
 [ -r ./build_env.sh ] && . ./build_env.sh
